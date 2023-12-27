c38cad1446a1e49514c78c3cbc6b4f502e519ac6
Neal <neal.easterling@gmail.com>
2023-12-27 09:10:23 -0600

    intial commit

---
+++ batch-updater/mit.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const controller = require('./controllers/svgController');
+
+const importFolderPath = "../public/images";
+const readFilePath = importFolderPath + "/";
+const importFolderDir = fs.readdirSync(importFolderPath);
+const outputFolderPath = './output/';
+let index = 0;
+
+function parseSVG(fileName){
+    index += 1;
+    let fileNameArray = fileName.split('-');
+    let title = fileNameArray[0];
+    const fileAsArray = controller.svgToArray(readFilePath + fileName);
+    const svgArray = controller.removeFill(fileAsArray);
+    controller.fixGroupElement(svgArray);
+    const lineAsString = controller.addClassAndAria(svgArray[0], title);
+    svgArray.splice(0, 1, lineAsString);
+    controller.addLicense(svgArray, 'MIT');
+    const newSVGasString = controller.parseArrayToString(svgArray);
+    controller.writeFile(newSVGasString, outputFolderPath + fileName);
+    console.log(`Completed ${index } of ${importFolderDir.length} `);
+}
+
+importFolderDir.forEach((fileName)=>{
+    parseSVG(fileName);
+    fs.unlinkSync(readFilePath + fileName);
+});
+
+
